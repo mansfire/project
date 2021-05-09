ac296e79e09f8ff051f62b456deb2f756c361dbb
mansfire <44177254+mansfire@users.noreply.github.com>
2021-05-09 13:40:04 -0600

    Update ZCz.m

--- ZCz.m
+++ ZCz.m
@@ -5,11 +5,11 @@ if x(1)==0
 else
     ZC_out=0;
 end
-z_prev=abs(x(1));
+z_prev=(x(1));
 for ii=2:length(x)
-    z_cur=abs(x(ii));
+    z_cur=(x(ii));
     z_dif=abs(z_cur-z_prev);
-    if z_cur<=e &&z_prev>e && z_dif>=e
+    if ((z_cur<=0 &&z_prev>0)|| (z_cur>0 &&z_prev<=0)) && z_dif>=e
         ZC_out=ZC_out+1;
     end
 end
