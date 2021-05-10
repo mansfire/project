6ef459aac0376a3820547a2b3d2f26bafb16ca3a
katyggray <kathryn.g.gray@cuanschutz.edu>
2021-05-10 16:52:00 -0600

    Add data separation, variable changes
    Added section that splits features into training and validation set.
    Adjusted variable names in LDA steps to reflect this.
    Added file save points for data sets.
    Spelling fixes

--- full_emg.m
+++ full_emg.m
@@ -1,4 +1,4 @@
-clc;
+% clc;
 clear;
 close all;
 
@@ -17,7 +17,9 @@ for idx = 1:numberOfFiles
     %mymodel.data{idx}{6} = zeros(length(mymodel.data{idx}{6}), 1); %channel 6 might be bad. Remove?
     mymodel.length{idx} = emgDataStruct.length_sec;
 end
-numberofChans= length(mymodel.data{1});%%find how many channels we have
+numberOfChans= length(mymodel.data{1});%%find how many channels we have
+
+
 %% Construct the filters, and run data through them.
 lp=480; % Hz
 hp=30; % Hz
@@ -43,7 +45,10 @@ for kk=1:numberOfFiles
     mymodel.meanChan{kk} = mean(fulldata,2);
 end
 
-%% add all the channels for each of the 11 test and validation postures
+save('filteredData.mat','mymodel','numberOfFiles','numberOfChans','fs','dt')
+%% add all the channels for each of the 12 test and validation postures
+% Not used? Artifact of deleted code??
+
 for ii=1:numberOfFiles
     total_data{idx}=sum(abs(mymodel.data{idx}), 2); % Rectifies the data, then adds all channels together.
 end
@@ -55,9 +60,13 @@ end
 mymodel = removeOffData(mymodel);
 TrimmedTF = mymodel.onData;
 
-% filename='trimmed.mat';
-% save(filename)
+save('trimmedData.mat','mymodel','TrimmedTF','numberOfFiles','numberOfChans','fs','dt')
+
+
 %% off data prep
+load('trimmedData.mat')
+
+
 binsize=0.05*fs;
 
 Tz = cell(numberOfFiles, 1);
@@ -67,37 +76,61 @@ end
 
 % extract each of our 4 choosen features, please see respective files
 for jj=1:numberOfFiles
-    nBin=floor(length(Tz{jj})/binsize); %%how many bins so we have 
+    nBin=floor(length(Tz{jj})/binsize); %%how many bins do we have 
     Bz=floor(linspace(1,length(Tz{jj}),nBin)); %%the bins themselves
     Ez=[];%%an empty matrix for intiallizing the other
-    ZC_TZ=Ez; %%zerocrossings
+    ZC_TZ=Ez; %%zero crossings
     SSC_TZ=Ez;%%slope sign changes
     MAV_TZ=Ez;%%mean absolute value
     WL_TZ=Ez;%% wavlength
-    for kk=1:numberofChans
-        
+    
+    for kk=1:numberOfChans    
         for ii=1:nBin-1
             Dz=Bz(ii+1);%%what bin we are in
             
-            ZC_TZ(kk,ii)=ZCz(Tz{jj}(kk,Bz(ii):Dz));%%find zero corrsing
+            ZC_TZ(kk,ii)=ZCz(Tz{jj}(kk,Bz(ii):Dz));%%find zero crossing
             MAV_TZ(kk,ii)=MAVz(Tz{jj}(kk,Bz(ii):Dz));%%find mean absolute value
             SSC_TZ(kk,ii)=SSCz(Tz{jj}(kk,Bz(ii):Dz));%%find slope sign change
-            WL_TZ(kk,ii)=WLz(Tz{jj}(kk,Bz(ii):Dz)); %%find wavelngth 
-            
+            WL_TZ(kk,ii)=WLz(Tz{jj}(kk,Bz(ii):Dz)); %%find wavelngth
         end
-        ZC{jj}=ZC_TZ;%%add the current ZC feature to the structure
-        MAV{jj}=MAV_TZ;%%add the current MAV feature to the structure
-        SSc{jj}=SSC_TZ;%%add the current SSC feature to the structure
-        WL{jj}=WL_TZ;%%add the current WL feature to the structure
-        
     end
     
+    ZC{jj}=ZC_TZ;%%add the current ZC feature to the structure
+    MAV{jj}=MAV_TZ;%%add the current MAV feature to the structure
+    SSC{jj}=SSC_TZ;%%add the current SSC feature to the structure
+    WL{jj}=WL_TZ;%%add the current WL feature to the structure
 end
 cord=0;
+
+%% Data separation
+trainingIndex = 1:2:24;
+validationIndex = 2:2:24;
+
+for i = 1:12
+    indexT = trainingIndex(i);
+    trainingNames{i} = mymodel.name{indexT};
+
+    indexV = validationIndex(i);
+    validationNames{i} = mymodel.name{indexV};
+end
+
+ZC_train = ZC(1, trainingIndex);
+MAV_train = MAV(1, trainingIndex);
+SSC_train = SSC(1, trainingIndex);
+WL_train = WL(1, trainingIndex);
+
+ZC_val = ZC(1, validationIndex);
+MAV_val = MAV(1, validationIndex);
+SSC_val = SSC(1, validationIndex);
+WL_val = WL(1, validationIndex);
+
+numberOfPoses = numberOfFiles / 2;
+
 %% covariance
-for ii=1:numberOfFiles
-    a=[WL{ii};SSc{ii};MAV{ii};ZC{ii}];%create a matrix of our feature
-    cord=cord+1; %%incriminent the index
+% TODO: covariance only on training data (todo)
+for ii=1:numberOfPoses
+    a=[WL_train{ii};SSC_train{ii};MAV_train{ii};ZC_train{ii}];%create a matrix of our feature
+    cord=cord+1; %%increment the index
     [M,N]=size(a);
     mn=mean(a,2);
     a=a-repmat(mn,1,N);
@@ -106,27 +139,32 @@ for ii=1:numberOfFiles
 
 end
 
-for ii=1:numberOfFiles
-
+for ii=1:numberOfPoses
+% TODO: W matrix only on training data (todo)
     u=mean(a_mat{ii}');
     bcs=cov(u'*u);%%between class
     wcs=zeros(32,32);%%lets do within class now
-    for jj=1:numberOfFiles
+    for jj=1:numberOfPoses
         wcs=wcs+cov_mat{jj};
     end
-    wcs=wcs/(numberOfFiles-1);
-    Op=wcs'*bcs;%%optimization matriz
+    wcs=wcs/(numberOfPoses-1);
+    Op=wcs'*bcs;%%optimization matrix
     [Z,W]=eig(Op);
     W=real(W);
  
     W=diag(W);%%look at the acual values
-    [val,index]=sort(-1*W);
+    [val,indexT]=sort(-1*W);
 
-    W=W(index);
-    Z=Z(:,index);
-    eig_vec{ii}=Z;%%eigne vector
+    W=W(indexT);
+    Z=Z(:,indexT);
+    eig_vec{ii}=Z;%%eigen vector
 
     Y{ii}=W'*a_mat{ii};%%use this to find euclidean
     
 end
 
+%% Euclidean Distance
+
+
+
+%% Classification
